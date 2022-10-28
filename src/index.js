ea5bac5f974f23cb323a89550994481f7128c14c
tsering651 <tseringwang4039@gmail.com>
2022-10-28 11:38:50 +0530

    Installed material-ui successfully

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+//import BasicCard from './screens/BasicCard';
 import Login from './screens/login/Login'
+
+
+
 
 ReactDOM.render(
    
--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -1,12 +1,18 @@
-import React from 'react'
-import Header from '../../Common/header/Header'
-
-
+//import React from 'react' 
+import * as React from 'react';
+import Alert from '@mui/material/Alert';
+import Stack from '@mui/material/Stack';
 
 export default function Login() {
   return (
-    <div>
-     <Header/>
-    </div>
+    <Stack sx={{ width: '100%' }} spacing={2}>
+    <Alert severity="error">This is an error alert — check it out!</Alert>
+    <Alert severity="warning">This is a warning alert — check it out!</Alert>
+    <Alert severity="info">This is an info alert — check it out!</Alert>
+    <Alert severity="success">This is a success alert — check it out!</Alert>
+    </Stack>
   )
 }
+
+
+
